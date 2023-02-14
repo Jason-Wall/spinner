c9efa89ea253bc72c46d87cfe9c7462d526e49a4
Jason-Wall <jason.waldick@gmail.com>
2023-02-14 09:48:55 -0700

    clean up old comments

--- spinner2.js
+++ spinner2.js
@@ -23,26 +23,3 @@ const spinner = (count) => {
 };
 
 spinner(5);
-
-
-
-
-
-    // setTimeout(() => {
-    //   process.stdout.write('\r|   ');
-    // }, revCount * freq + (0 / 4 * freq));
-
-    // setTimeout(() => {
-    //   process.stdout.write('\r/   ');
-    // }, revCount * freq + (1 / 4) * freq);
-
-    // setTimeout(() => {
-    //   process.stdout.write('\r-   ');
-    // }, revCount * freq + (2 / 4) * freq);
-
-    // setTimeout(() => {
-    //   // Need to escape the backslash since it's a special character.
-    //   process.stdout.write('\r\\   ');
-    // }, revCount * freq + (3 / 4) * freq);
-
-    // revCount++;
